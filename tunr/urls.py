8b4b4aa418054943aeea5319867eb6cc0202fc27
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2019-04-02 15:18:51 -0400

    fix url bug, add pipfile, remove vs code settings

--- tunr/urls.py
+++ tunr/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
     path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
 
-
-    path('songs', views.song_list, name='song_list'),
+    path('songs/', views.song_list, name='song_list'),
     path('songs/<int:pk>', views.song_detail, name='song_detail'),
     path('songs/new', views.song_create, name='song_create'),
     path('songs/<int:pk>/edit', views.song_edit, name='song_edit'),
